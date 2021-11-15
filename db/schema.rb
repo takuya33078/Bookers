7e7d26beff3460af28b1740a2bd4231bb8fefa68
takuya33078 <ec2-user@ip-172-31-40-58.ec2.internal>
2021-11-15 12:55:54 +0000

    [finish]Bookers

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_124811) do
+ActiveRecord::Schema.define(version: 2021_08_17_150344) do
 
   create_table "books", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
